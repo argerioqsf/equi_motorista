88c9c9a885c4ce51d56c61219d5ae50f6c5cf91d
argerioqsf <argerioaf3@gmail.com>
2018-07-05 17:39:34 -0300

    credenciais alteradas

--- src/app/credentials.ts
+++ src/app/credentials.ts
@@ -1,8 +1,8 @@
 export const FIREBASE_CREDENTIALS = {
-    apiKey: "AIzaSyAFURA4FpZcSwCPZQMGspB3GlwjXCpd38c",
-    authDomain: "taxi-831b2.firebaseapp.com",
-    databaseURL: "https://taxi-831b2.firebaseio.com",
-    projectId: "taxi-831b2",
-    storageBucket: "taxi-831b2.appspot.com",
-    messagingSenderId: "692625356397"
+    apiKey: "SUAS CREDENCIAIS",
+    authDomain: "SUAS CREDENCIAIS",
+    databaseURL: "SUAS CREDENCIAIS",
+    projectId: "SUAS CREDENCIAIS",
+    storageBucket: "SUAS CREDENCIAIS",
+    messagingSenderId: "SUAS CREDENCIAIS"
 };
